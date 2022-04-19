257d0f38dbfddd25a07b62312596b09a5eec8f25
Carolina Nonato <csnonato@gmail.com>
2022-04-19 17:32:42 -0400

    Showing posts in home page

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ get_header();
 if ( have_posts () ) :
     while ( have_posts() ) :
         the_post();
-        //do our stuff
+        get_template_part( 'template-parts/post/content' );
     endwhile;
 
     endif;
