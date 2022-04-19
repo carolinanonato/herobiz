edd52248e7c923244413fe6ba4e2dd7b0100ee0a
Carolina Nonato <csnonato@gmail.com>
2022-04-19 16:50:26 -0400

    header and footer already working

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Containr the header. 
+ */
+
+?><!doctype html>
+<html>
+    <head>
+        <meta charset="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    
+</body>
+
+    </html>
