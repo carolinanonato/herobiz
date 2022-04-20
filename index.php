af66555b4a695ec8ba9d3acb87240b574667fff3
Carolina Nonato <csnonato@gmail.com>
2022-04-19 21:37:57 -0400

    implement single page

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
 /**
- * 
+ * The main template file.
  */
 
-
 get_header();
-if ( have_posts () ) :
-    while ( have_posts() ) :
-        the_post();
-        get_template_part( 'template-parts/post/content' );
-    endwhile;
-
+    if ( have_posts() ) :
+        while ( have_posts() ) :
+            the_post();
+            get_template_part( 'template-parts/post/content' );
+        endwhile;
     endif;
 get_footer();
-
